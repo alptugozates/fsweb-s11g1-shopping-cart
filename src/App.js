6592cf92da11f704b43a02c204b6f23e5727c33f
alptugozates <omeralptugozates@gmail.com>
2023-11-27 20:43:47 +0300

    context'leri provider ile wrapler storage kaydeder

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
+import { ProductContext } from "./contexts/ProductContext";
 
 // Bileşenler
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
+    return storedCart;
+  });
 
   const addItem = (item) => {
-    // verilen itemi sepete ekleyin
+    const isAlreadyInCart = cart.find((cartItem) => cartItem.id === item.id);
+    if (!isAlreadyInCart) {
+      setCart([...cart, item]);
+    } else {
+      setCart([...cart]);
+    }
+
   };
+  const removeItem = (id) => {
+    setCart([...cart.filter((item) => item.id !== id)])
+  }
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }} >
+        <CartContext.Provider value={{ cart, removeItem }} >
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+          <Navigation cart={cart} />
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
+          {/* Routelar */}
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
+
+            <Route path="/cart">
+              <ShoppingCart cart={cart} />
+            </Route>
+          </main>
+        </CartContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
